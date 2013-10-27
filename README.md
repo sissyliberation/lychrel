da7fc33fad18bb6e779f614a6b2c7c326e119fb4
Yves Courtois <Courtois1337@gmail.com>
2013-10-26 20:34:03 -0500

    updated everything

--- README.md
+++ README.md
@@ -2,3 +2,15 @@ lychrel
 =======
 
 Lychrel Number Calculator
+
+Description
+-----------
+
+Lychrel Number Calculator done in JavaScript.
+
+You can learn more about Lychrel Numbers [here](http://en.wikipedia.org/wiki/Lychrel_number).
+
+Demo
+-----------------
+
+You can view it [here](http://yvescourtois.com/lychrel/).
--- lychrel.js
+++ lychrel.js
@@ -3,9 +3,7 @@ var holder="";
 function reverse(s){ return s.split("").reverse().join(""); } /* reverse a string */
 
 function lychrel (num,count) { /* calculates palindrome or adds then go */
-	if(count==1) {
-	holder=num;concat+="/-------------------------------------------------------------------------------------------------------------/</br></br>";
-	}
+
 	concat+=count+". "+num+" + " + reverse(num.toString());
 	concat+=" = "+ (num+parseInt(reverse(num.toString())) )+"</br>";
 	num += parseInt(reverse(num.toString()));
@@ -14,17 +12,20 @@ function lychrel (num,count) { /* calculates palindrome or adds then go */
 		concat+="</br>The palyndrome for "+holder+ " is "+num+" , formed after "+count+ " iterations.</br></br>";
 		return num;	
 	}
-	else {	lychr
-	el(num,count+1); }
+	else {	lychrel(num,count+1); }
 	return num;
 }
 
-function isNumber (o) { return ! isNaN (o-0) && o !== null && o !== "" && o !== false; }
+function isNumber (o) {
+  return ! isNaN (o-0) && o !== null && o !== "" && o !== false;
+}
 
 function start() {
-
+	
+	concat="</br>";
 	var number=prompt("Please choose a natural number","Enter natural number here.");
 	if (isNumber(number)) {
+		holder = number;
 		lychrel(parseInt(number),1);
 		document.getElementById("demo").innerHTML=concat;
 	}
